cf19f39d1957a22008163fd696fdf394cb1bd577
Shubhamoy <shubhamoys@gmail.com>
2025-06-22 11:53:16 +0530

    ~ Enhancement: Splash screen before loading theme

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todo_ui_flutter/core/widgets/splash/splash_screen.dart';
 import 'core/theme/app_theme.dart';
 import 'core/theme/theme_provider.dart';
 import 'package:todo_ui_flutter/config/routes/app_router.dart';
@@ -9,6 +10,18 @@ class App extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final themeReady = ref.watch(themeReadyProvider);
+
+    if (themeReady.isLoading) {
+      // Wrap SplashScreen in MaterialApp to provide Directionality
+      return MaterialApp(
+        theme: AppTheme.lightTheme,
+        darkTheme: AppTheme.darkTheme,
+        themeMode: ThemeMode.system,
+        home: const SplashScreen(),
+      );
+    }
+
     final themeMode = ref.watch(themeProvider);
 
     return MaterialApp.router(
