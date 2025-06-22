c47bd977d4bdf0f95838166b69796592c55b840d
Shubhamoy <shubhamoys@gmail.com>
2025-06-22 12:41:38 +0530

    ~ Enhancement: App Icon updated

--- lib/app.dart
+++ lib/app.dart
@@ -25,7 +25,7 @@ class App extends ConsumerWidget {
     final themeMode = ref.watch(themeProvider);
 
     return MaterialApp.router(
-      title: 'Todo App',
+      title: 'UpToDo',
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
       themeMode: themeMode, // Use ThemeMode from provider
